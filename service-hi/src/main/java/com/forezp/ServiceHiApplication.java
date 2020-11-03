e45ddff9b231175f731c2f2a9f31a21d427edb1e
xcy <xcy123456>
2020-11-03 09:47:45 +0800

    1

--- service-hi/src/main/java/com/forezp/ServiceHiApplication.java
+++ service-hi/src/main/java/com/forezp/ServiceHiApplication.java
@@ -1,27 +1,65 @@
 package com.forezp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author CAMIT
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class ServiceHiApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHiApplication.class, args);
 	}
 
+	private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
 	@Value("${server.port}")
 	String port;
-	@RequestMapping("/hi")
+	@RequestMapping("/hi2")
 	public String home(@RequestParam String name) {
 		return "hi "+name+",i am from port:" +port;
 	}
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/hi")
+	public String callHome(){
+		LOG.log(Level.INFO, "calling trace service-hi  ");
+		return restTemplate.getForObject("http://192.168.10.188:8989/miya", String.class);
+	}
+	@RequestMapping("/info")
+	public String info(){
+		LOG.log(Level.INFO, "calling trace service-hi ");
+
+		return "i'm service-hi";
+
+	}
+	@Bean
+	public AlwaysSampler defaultSampler(){
+		return new AlwaysSampler();
+	}
 }
